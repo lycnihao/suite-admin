deffa239261d8c8db11a438ae6332f0232de37a4
liyc <38707145@qq.com>
2023-02-15 13:41:16 +0800

    新增:全局异常处理类

---
+++ suite-admin-api/src/main/java/net/koodar/suite/admin/exception/handler/ControllerExceptionHandler.java
@@ -0,0 +1,43 @@
+package net.koodar.suite.admin.exception.handler;
+
+import io.jsonwebtoken.lang.Assert;
+import lombok.extern.slf4j.Slf4j;
+import net.koodar.suite.admin.exception.ServiceException;
+import net.koodar.suite.admin.model.support.BaseResponse;
+import net.koodar.suite.admin.util.ExceptionUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 异常处理
+ *
+ * @author liyc
+ */
+@Slf4j
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+	@ExceptionHandler(ServiceException.class)
+	public ResponseEntity<BaseResponse<Object>> handleServiceException(ServiceException e) {
+		BaseResponse<Object> jsonResult = handleBaseException(e);
+		jsonResult.setCode(e.getStatus().value());
+		jsonResult.setData(e.getErrorData());
+		return new ResponseEntity<>(jsonResult, e.getStatus());
+	}
+
+	private BaseResponse<Object> handleBaseException(Throwable t) {
+		Assert.notNull(t, "Throwable must not be null");
+
+		log.error("Captured an exception", t);
+
+		BaseResponse<Object> jsonResult = new BaseResponse<>();
+		jsonResult.setMessage(t.getMessage());
+		if (log.isDebugEnabled()) {
+			jsonResult.setDevMessage(ExceptionUtils.getStackTrace(t));
+		}
+
+		return jsonResult;
+	}
+
+}
